e429fc65242dd7577156924851a16421f34810ab
Atunde <130572243+Atunde-SS@users.noreply.github.com>
2025-09-25 13:47:27 +0100

    Add files via upload

---
+++ Example-Website/abi/ZamaDiceAddresses.ts
@@ -0,0 +1,7 @@
+/*
+  Zama Dice Game Contract Addresses
+*/
+export const GzamaAddresses = { 
+  "11155111": { address: "0xAA89C8E13A0Bb610F5320D022679eAF85495C1C4", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0x1404998dE4F70608570D12f54faC6e30Ef01D63c", chainId: 31337, chainName: "hardhat" },
+};
